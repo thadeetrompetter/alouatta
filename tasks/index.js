f1e27cc12e28c52048d782adb4aea771f50d5631
Thadee Trompetter <thadee@voorhoede.nl>
2015-09-06 14:58:58 +0200

    allow task variations to be added

--- tasks/index.js
+++ tasks/index.js
@@ -1,21 +1,45 @@
-var gulp = require('gulp');
+var assign = require('lodash.assign'),
+    gulp = require('gulp'),
+    gutil = require('gulp-util');
 
 module.exports = function getTasks(tasks) {
     if(!tasks){
         return gulp;
     }
-    tasks.forEach(function (task) {
-        var name = task.name,
-            dependencies = task.dependencies,
-            signature = [ task.name ],
-            fn;
-        if(typeof name === 'string'){
-            fn = require(['.', name].join('/'));
-            if(Array.isArray(dependencies) && dependencies.length){
-                signature = signature.concat([ dependencies ]);
-            }
-            gulp.task.apply(gulp, signature.concat(fn));
-        }
-    });
+    tasks.forEach(getTask);
     return gulp;
 };
+
+function getTask(task) {
+    var name = task.name,
+        variations = task.variations;
+
+    if(typeof name === 'string'){
+        task.fn = require(['.', name].join('/'));
+        appendTask(task);
+        if(variations && variations.length){
+            variations.forEach(function (variation) {
+                variation = assign({
+                    fn: task.fn
+                }, variation);
+                appendTask(variation, name);
+            });
+        }
+    }
+}
+function appendTask(task, groupName) {
+    var args = task.args,
+        dependencies = task.dependencies,
+        signature = [ groupName ? [groupName, task.name].join(':') : task.name ];
+    if(args){
+        task.fn = task.fn.bind(null, args);
+    }
+    if(Array.isArray(dependencies) && dependencies.length){
+        signature = signature.concat([ dependencies ]);
+    }
+    gulp.task.apply(gulp, signature.concat(task.fn));
+    gutil.log(
+        gutil.colors.yellow('Registered task:'),
+        gutil.colors.green(signature.slice(0,1)[0])
+    );
+}

7647b6ea8bb80e73c93e93b186cbc31520c21963
Thadee Trompetter <thadee@voorhoede.nl>
2016-10-09 13:04:36 +0200

    close menu on click outside header

--- source/components/navigation/navigation.js
+++ source/components/navigation/navigation.js
@@ -1,14 +1,25 @@
 'use strict';
 
+var MENU_OPEN_CLASS = 'menu-open';
+
 function Navigation(element) {
     if(!element){
         return;
     }
     this.element = element;
     this.handle = document.querySelector('[data-menu-handle]');
+    this.header = document.querySelector('[data-header]');
 
-    this.handle.addEventListener('click', function (event) {
-        document.body.classList.toggle('menu-open');
+    this.handle.addEventListener('click', function () {
+        document.body.classList.toggle(MENU_OPEN_CLASS);
+    });
+
+    this.header.addEventListener('click', function (event) {
+        event.stopPropagation();
+    });
+
+    document.documentElement.addEventListener('click', function () {
+        document.body.classList.remove(MENU_OPEN_CLASS);
     });
 }
 new Navigation(document.querySelector('[data-navigation]'));
